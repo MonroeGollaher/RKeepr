ea5b2f8943e4547aba04c499c79984d8239f2530
Monroe Gollaher <mgollaher92@gmail.com>
2021-02-08 16:13:46 -0700

    implement auth0 authentication

--- reactKeepr.client/src/App.js
+++ reactKeepr.client/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
+  const { isLoading } = useAuth0();
 
+  if (isLoading) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+  
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
   );
 }
 
